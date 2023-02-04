efac837f8126ceb73199bc8c84e9531f6609fc02
Moonlight Dorkreamer 3 <124145361+Dorkreamer@users.noreply.github.com>
2023-02-04 03:28:21 -0700

    Little toggles for the bot

---
+++ settings.py
@@ -0,0 +1,11 @@
+class modules:
+    music = True
+    cli = True
+    scraping = True
+    moderation = True
+    fun = True
+
+activity = "playing"
+status = "you nerds"
+
+mode = "Development" #Development, Retail
