e08301e6a631d061eda8fe14517346325624558b
Moonlight Dorkreamer 3 <124145361+Dorkreamer@users.noreply.github.com>
2023-05-05 06:46:58 -0700

    V2's new guild_settings!
    Now storing guild_settings using .json files!

--- guild_settings.py
+++ guild_settings.py
@@ -1,94 +1,58 @@
+from objdict import ObjDict
+import objdict
 import os
-import configparser
 
-import localization.enus as English
-import localization.ja as Japanese
-import localization.ptbr as Portuguese
-import localization.uwuify.enus as EnglishUwU
+def new_guild(id):
+    data = ObjDict()
+    if not os.path.isdir("./guild_data"):
+        os.makedirs("./guild_data")
+    f = open(f"./guild_data/{id}.json","w")
+    f.write(objdict.dumps(data))
+    f.close()
 
-def set_language(id,language):
-    config = configparser.ConfigParser()
-    if not os.path.isfile(f"./data/guilds{id}.ini"):
-        if not os.path.isdir("./data/guilds"):
-            os.makedirs("./data/guilds")
-        config.add_section("Info")
-        config["Info"]["GuildID"] = str(id)
-        config.add_section("locale")
-    config["locale"]["language"] = language
-    with open(f"./data/guilds/{id}.ini","w") as f:
-        config.write(f)
-
-def guild_language(id):
-    config = configparser.ConfigParser()
-    try:
-        config.read_file(open(rf'./data/guilds/{id}.ini'))
-        return config.get('locale', 'language')
-    except:
-        set_language(id, "English")
-        return "English"
-
-def get_uwu(id):
-    config = configparser.ConfigParser()
-    try:
-        config.read_file(open(rf'./data/guilds/{id}.ini'))
-        if config.get('locale', 'uwu') == "True":
-            return True
-        else:
-            return False
-    except:
-        set_uwu(id,False)
-        return False
-
-def set_uwu(id,enabled=False):
-    config = configparser.ConfigParser()
-    if guild_language(id) == "English":
-        config.read_file(open(rf'./data/guilds/{id}.ini'))
-        config.set("locale","uwu",f"{str(enabled)}")
-        with open(f"./data/guilds/{id}.ini","w") as f:
-            config.write(f)
+def set(id,option,value):
+    if not os.path.isdir("./guild_data"):
+        os.makedirs("./guild_data")
+    if not os.path.exists(f"./guild_data/{id}.json"):
+        new_guild(id)
+        data = ObjDict()
     else:
-        return "English Only"
+        with open(f"./guild_data/{id}.json","r") as guild_data:
+            try:
+                data = objdict.loads(str(guild_data.read()))
+            except objdict.JsonDecodeError:
+                new_guild(id)
+                with open(f"./guild_data/{id}.json","r") as guild_data:
+                    data = objdict.loads(str(guild_data.read()))
+    data[option] = value
+    with open(f"./guild_data/{id}.json","w") as guild_data:
+        guild_data.write(objdict.dumps(data))
 
-def text(string,lang,uwu=False):
-    if lang == "Portuguese":
-        locale = Portuguese.locale
-    elif lang == "Japanese":
-        locale = Japanese.locale
-    elif lang == "English":
-        if uwu:
-            locale = EnglishUwU.locale
-        else:
-            locale = English.locale
-    else:
-        return "Invalid Language!"
-    return str(locale.get(string))
-
-def set(id,section,option,value=None,default=None):
-    config = configparser.ConfigParser()
-    if not os.path.isfile(f"./data/guilds{id}.ini"):
-        set_language(id,"English")
-    if not value:
-        value = default
+def read(id,option,default=None):
     try:
-        config.read_file(open(rf'./data/guilds/{id}.ini'))
-        config.set(section,option,value)
-    except configparser.NoSectionError:
-        config.add_section(f"{section}")
-        #config.set(section,option,value)
-        config[f"{section}"][f"{option}"] = value
-    with open(f"./data/guilds/{id}.ini","w") as f:
-        config.write(f)
-        f.close()
-
-def read(id,section,option,create=False,create_default=None):
-    config = configparser.ConfigParser()
-    try:
-        config.read_file(open(rf'./data/guilds/{id}.ini'))
-        return config.get(str(section), str(option))
-    except configparser.NoSectionError:
-        if create:
-            set(id,section,option,create_default)
-            config.read_file(open(rf'./data/guilds/{id}.ini'))
-            return config.get(f'{section}',f'{option}')
-        else:
-            return None
+        with open(f"./guild_data/{id}.json","r") as guild_data:
+            data = objdict.loads(str(guild_data.read()))
+            try:
+                return data[option]
+            except KeyError:
+                if default == None:
+                    return Exception(f"The guild ({id}) does not contain '{option}' stored in it's settings!")
+                else:
+                    set(id,option,default)
+                    return default
+    except FileNotFoundError:
+        new_guild(id=id)
+    
+def remove(id,option):
+    if not os.path.isdir("./guild_data"):
+        os.makedirs("./guild_data")
+    if not os.path.exists(f"./guild_data/{id}.json"):
+        raise Exception("There's no data for this guild!")
+    with open(f"./guild_data/{id}.json","r") as guild_data:
+        data = objdict.loads(str(guild_data.read()))
+    with open(f"./guild_data/{id}.json","w") as guild_data:
+        try:
+            del data[option]
+            guild_data.write(objdict.dumps(data))
+        except KeyError:
+            pass
