1f565e99d8c4644d0ceb192cdd812c596534177d
Moonlight Dorkreamer 3 <124145361+Dorkreamer@users.noreply.github.com>
2023-05-12 20:07:25 -0700

    Update music.py
    New commands:
    `/nightcore`
    `/karaoke`
    
    Bugfixes:
    `/play` not playing YouTube links and/or being inconsistent at it.
    
    Misc:
    `/play` now has a new optional argument called `voice_channel`, which let's you choose a voice channel without being in it.

--- modules/music.py
+++ modules/music.py
@@ -10,6 +10,7 @@ import asyncio
 import settings
 from os import getenv
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -83,7 +84,8 @@ class music(commands.GroupCog, name="music"):
                     await payload.player.play(payload.player.queue[payload.player.queue.find_position(last_track)],replace=True)
                 else:
                     await payload.player.stop()
-
+        # if payload.player.queue.find_position(last_track)+1 >= int(settings.music.max_rewind):
+        #     del payload.player.queue[0]
 
     #/resume
     @app_commands.command(name="resume",description="Resumes the song's playback")
@@ -208,21 +210,80 @@ class music(commands.GroupCog, name="music"):
         
         await interaction.response.send_message(embed=emb,view=PlayerView())
 
+    #/karaoke
+    @app_commands.command(name="karaoke",description="| Music | Muffles the vocals, so yours can shine... or rather shrill")
+    async def karaoke(self, interaction: discord.Interaction):
+        node = wavelink.NodePool.get_node()
+        player = node.get_player(interaction.guild_id)
+
+        try:
+            vc: wavelink.Player = interaction.guild.voice_client
+        except:
+            return await interaction.followup.send("I'm not connected to any voice channels ¯\_(ツ)_/¯")
+        
+        try:
+            if "timescale" in player.filter:
+                await player.set_filter(wavelink.Filter(karaoke=wavelink.Karaoke(level=0)))
+                return await interaction.response.send_message("Karaoke *Disabled!*")
+            else:    
+                await player.set_filter(wavelink.Filter(karaoke=wavelink.Karaoke(level=1)))
+                return await interaction.response.send_message("**Karaoke Enabled!!**")
+        except AttributeError:
+            await player.set_filter(wavelink.Filter(karaoke=wavelink.Karaoke(level=1)))
+            return await interaction.response.send_message("**Karaoke Enabled!!**")
+
+
+    #/nightcore
+    @app_commands.command(name="nightcore",description="| Music | Makes everything faster and cuter... I guess?")
+    async def nightcore(self, interaction: discord.Interaction):
+        node = wavelink.NodePool.get_node()
+        player = node.get_player(interaction.guild_id)
+
+        try:
+            vc: wavelink.Player = interaction.guild.voice_client
+        except:
+            return await interaction.followup.send("I'm not connected to any voice channels ¯\_(ツ)_/¯")
+        
+        try:
+            if "timescale" in player.filter:
+                await player.set_filter(wavelink.Filter(timescale=wavelink.Timescale(speed=1,pitch=1)))
+                return await interaction.response.send_message("Nightcore *Disabled!*")
+            else:    
+                await player.set_filter(wavelink.Filter(timescale=wavelink.Timescale(speed=1.8,pitch=1.9)))
+                return await interaction.response.send_message("**Nightcore Enabled!!**")
+        except AttributeError:
+            await player.set_filter(wavelink.Filter(timescale=wavelink.Timescale(speed=1.8,pitch=1.9)))
+            return await interaction.response.send_message("**Nightcore Enabled!!**")
+
+    #/move
+    #@app_commands.command(name="move",description="| Music | Moves the current playback to another voice channel!")
+
     #/play
     @app_commands.command(name="play",description="| Music | Searches/Plays a music URL from YouTube, YouTube Music, Spotify, SoundCloud")
-    async def play(self, interaction: discord.Interaction, *, query: str):
+    @app_commands.describe(voice_channel="Which voice channel do you want to play it, instead of your own")
+    async def play(self, interaction: discord.Interaction, *, query: str, voice_channel:Optional[discord.VoiceChannel]):
         await interaction.response.defer(thinking=True)
+        query = re.sub(re.escape("www."),"",query) #This avoids so many weird problems, it's unbelievable
+
+        #Define some basic variables
         playlist = False
         spotify_query = False
         spotify_type = None
-        try:
-            if not interaction.guild.voice_client:
-                vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player)
-            else:
-                vc: wavelink.Player = interaction.guild.voice_client
-        except:
-            return await interaction.followup.send("I'm not connected to any voice channels ¯\_(ツ)_/¯")
+        
+        #Check if the user is in any voice channel
+        if voice_channel:
+            vc: wavelink.Player = await voice_channel.connect(cls=wavelink.Player)
+        else:
+            try:
+                if not interaction.guild.voice_client:
+                    vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player)
+                else:
+                    vc: wavelink.Player = interaction.guild.voice_client
+            except:
+                return await interaction.followup.send("I'm not connected to any voice channels ¯\_(ツ)_/¯")
+            vc.autoplay = False #Autoplay sucks, disable it
 
+        #Format YouTube Music links to regular ones, usually avoids a ton of problems
         if any(["music.youtube.com/playlist" in query]):
             query = re.sub(re.escape("https://music.youtube.com"),"https://youtube.com",query)
             query = re.sub(re.escape("http://music.youtube.com"),"https://youtube.com",query)
@@ -230,9 +291,13 @@ class music(commands.GroupCog, name="music"):
             playlist = True
         elif any(["music.youtube.com" in query]):
             track = await wavelink.YouTubeMusicTrack.search(query,return_first=True)
+        
+        #Set the flag for playlists if there's any YouTube playlist and searches accordingly
         elif any(["playlist?list=" in query]):
             track = await wavelink.YouTubePlaylist.search(query)
             playlist = True
+        
+        #Set the flags for playlists/albums from Spotify to search accordingly
         elif any(["open.spotify.com" in query]):
             spotify_query = True
             decoded = spotify.decode_url(query)
@@ -247,47 +312,68 @@ class music(commands.GroupCog, name="music"):
             elif decoded and decoded['type'] is spotify.SpotifySearchType.album:
                 playlist = True
                 spotify_type = spotify.SpotifySearchType.album
+        
+        #Just any regular search that doesn't match the patterns above
         else:
             try:
-                track = await wavelink.YouTubeTrack.search(query, return_first=True)
-            except wavelink.NoTracksError:
-                print(f"{query} wasn't found") #Implement an actual response to this later
+                track = await wavelink.YouTubeTrack.search(query, return_first=True) #Regular search
+            except:
+                try:
+                    track = await wavelink.YouTubeMusicTrack.search(query, return_first=True) #YT Music search
+                except (wavelink.exceptions.WavelinkException, wavelink.NoTracksError):
+                    try:
+                        if any(["https://youtube.com" in query]): #Check if the query is a YouTube URL
+                            track = await wavelink.YouTubeTrack.search(f"{pytube.YouTube(query).title} {pytube.YouTube(query).author}",return_first=True) #Searches using the video's title instead (solves the "Track failed to load." issue)                 
+                    except wavelink.NoTracksError: #In case it's hopeless
+                        return await interaction.followup.send("Unfortunately this song couldn't be found!")
 
-        vc.autoplay = False
+        #Check if there's already something playing
         if vc.is_playing():
-            counter = 0
+            counter = 0 #Little cutie counter
+            
+            #Check the Spotify and playlist flag
             if spotify_query and playlist:
+                #Iterate through the playlist/album
                 for track in await spotipy.get(type=spotify_type,query=query):
                     try:
                         await vc.queue.put_wait(await wavelink.YouTubeTrack.search(track,return_first=True))
                         counter+=1
                     except wavelink.NoTracksError:
-                        pass
+                        await interaction.followup.send(f"Couldn't find {track} on YouTube!")
                 await interaction.followup.send(embed=discord.Embed(title=f"**{counter} Songs!**",description=f"From the Spotify playlist/album:[{await spotipy.list_name(spotify_type,query)}]({query})").set_author(name="Added to queue"))
+            #If it's a playlist but not a spotify one
             elif playlist and not spotify_query:
                 counter = 0
+                #Iterate through the tracks with a little counter again
                 for track in track.tracks:
                     vc.queue.put(track)
                     counter+=1
                 await interaction.followup.send(embed=discord.Embed(title=f"**{counter} songs!**",description=f"From the playlist/album: **[{Playlist(query).title}]({query})**").set_author(name="Added to queue:"))
             else:
+                #In case it's neither a playlist nor from Spotify
                 vc.queue.put(track)
                 try:
-                    await interaction.followup.send(embed=discord.Embed(title=f"**{track.title}**",url=track.uri).set_author(name="Added to queue").set_thumbnail(url=track.thumbnail))
+                    await interaction.followup.send(embed=discord.Embed(title=f"**{track.title}**",url=track.uri).set_author(name="Added to queue").set_thumbnail(url=str(pytube.YouTube(track.uri).thumbnail_url)))
                 except:
                     await interaction.followup.send(embed=discord.Embed(title=f"**{track.title}**",url=track.uri).set_author(name="Added to queue"))
 
+        #If there's nothing playing
         else:
+            #Check if it's a spotify query and playlist
             if spotify_query and playlist:
                 counter = 0
+                #Iterate through playlist/album
                 for track in await spotipy.get(type=spotify_type,query=query):
+                    #If it's the first song
                     if counter < 1:
+                        #Tries to load the first song from the list before adding anything else, avoids an unecessary delay to start playing
                         try:
                             await vc.queue.put_wait(await wavelink.YouTubeTrack.search(track,return_first=True))
                             await vc.play(vc.queue[0])
                             counter+=1
                         except wavelink.NoTracksError:
                             pass
+                    #For the rest of the songs...
                     else:
                         try:                
                             await vc.queue.put_wait(await wavelink.YouTubeTrack.search(track,return_first=True))
@@ -299,11 +385,14 @@ class music(commands.GroupCog, name="music"):
                 except:
                     await interaction.followup.send(embed=discord.Embed(title=f"**{vc.current.title}**",description=f"From the Spotify playlist/album: [{await spotipy.list_name(spotify_type,query)}]({query})",url=vc.current.uri).set_author(name="Now playing:"))
 
+            #If it's a playlist but not from Spotify
             elif playlist and not spotify_query:
                 for track in track.tracks:
                     vc.queue.put(track)
                 await vc.play(vc.queue.get())
                 await interaction.followup.send(embed=discord.Embed(title=f"{vc.current.title}",description=f"From the playlist: **[{Playlist(query).title}]({query})**",url=vc.current.uri).set_author(name="Now playing:").set_thumbnail(url=pytube.YouTube(vc.current.uri).thumbnail_url))
+            
+            #Else if it's neither a playlist nor from Spotify
             else:
                 vc.queue.put(track)
                 await vc.play(track)
@@ -412,6 +501,6 @@ class music(commands.GroupCog, name="music"):
             await ctx.response.send_message("Okay! Stopped the current playback!")
 
 async def setup(bot: commands.Bot) -> None:
-    node: wavelink.Node = wavelink.Node(uri='http://localhost:2333', password='')
+    node: wavelink.Node = wavelink.Node(uri='http://localhost:2333', password='dummythicc')
     await wavelink.NodePool.connect(client=bot, nodes=[node],spotify=sc)
     await bot.add_cog(music(bot))
