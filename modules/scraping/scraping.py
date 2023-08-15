2ead8a9e197bcdd9bae50e2060c980dc75b14b97
Moonlight Dorkreamer 3 <124145361+Dorkreamer@users.noreply.github.com>
2023-08-14 18:45:39 -0700

    Update scraping.py
    - Now `/reddit` has a safe filter!
    - function `get_reddit` is now fully async
    - `/gelbooru`'s safe mode, is now provided by the website tagging and not Eden AI
    '`/gelbooru` Tags now work as they should on a booru, being split by spaces

--- modules/scraping/scraping.py
+++ modules/scraping/scraping.py
@@ -25,20 +25,62 @@ import random
 import imagetools
 import settings
 
+import httpx
+from bs4 import BeautifulSoup
+
 import modules.scraping.steam as steam
 
 #Define gelbooru with its credentials
 gelbooru = Gelbooru(os.getenv("gelbooru_api_key"), os.getenv("gelbooru_user_id"))
 
 #Gets reddit posts with the defined boundaries
-def get_reddit(subreddit,count,listing,timeframe):
-    try:
-        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?count={count}&t={timeframe}'
-        request = requests.get(base_url, headers = {'User-agent': 'Xyn'})
-    except:
-        print('An Error Occured')
-        raise Exception("Error getting the sub")
-    return request.json()
+async def random_reddit(subreddit, safe_mode=True, max_retries=3):
+    retries = 0
+    while retries < max_retries:
+        try:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+            }
+            async with httpx.AsyncClient() as client:
+                url = f"https://www.reddit.com/r/{subreddit}/random.json?count=1&t=all"
+                response = await client.get(url, headers=headers)
+
+                if response.status_code == 302:
+                    redirect_url = response.headers.get("location")
+                    if redirect_url:
+                        response = await client.get(redirect_url, headers=headers)
+
+                response.raise_for_status()
+
+                # Continue with parsing the response
+                data = response.json()
+                if data and isinstance(data, list):
+                    post_data = data[0]['data']['children'][0]['data']
+                    title = post_data['title']
+                    url = post_data['url']
+                    nsfw = post_data['over_18']
+                    
+                    if safe_mode and nsfw:
+                        retries += 1
+                        continue
+                    
+                    return {
+                        'title': title,
+                        'url': url,
+                        'nsfw': nsfw
+                    }
+
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 302:
+                print("Redirecting...")
+            else:
+                print(f"HTTP Error: {e}")
+            retries += 1
+        except Exception as e:
+            print(f"Error: {e}")
+            retries += 1
+
+    return None
 
 class steam_cog(commands.GroupCog,name="steam"):
     def __init__(self, bot: commands.Bot) -> None:
@@ -131,97 +173,41 @@ class scr(commands.GroupCog, name="scraping"):
 
     #/reddit
     @app_commands.command(name="reddit",description="| Scraping | Gets a random post from a subreddit")
-    async def reddit(self, interaction: discord.Interaction, subreddit: str,) -> None:
-        subreddit = subreddit = re.sub("r/","",subreddit)
-        try:
-            top_post = get_reddit(subreddit,count=1,listing="random",timeframe="all")
-        except Exception("Error getting the sub"):
-            await interaction.response.send_message("There was an error fetching from this subreddit, check your spelling and/or try again later")
-            return
-        try:
-            title = top_post[0]['data']['children'][0]['data']['title']
-            url = top_post[0]['data']['children'][0]['data']['url']
-        except KeyError:
-            await interaction.response.send_message("This subreddit wasn't found, are you sure you spelled it correctly?")
-            return
+    async def reddit(self, interaction: discord.Interaction, subreddit: str, safe_mode:Literal["Yes","No"]) -> None:
+        await interaction.response.defer(thinking=True)
         
-        await interaction.response.send_message(f'{title}\n{url}')
+        if safe_mode == "No":
+            if interaction.guild and not interaction.channel.is_nsfw():
+                return await interaction.followup.send("The Safe Mode can only be turned off in a NSFW channel!")
+        
+        subreddit = re.sub(r"r/", "", subreddit)
+        try:
+            random_post = await random_reddit(subreddit,{"Yes":True,"No":False}.get(safe_mode,True))
+            if random_post:
+                title = random_post['title']
+                url = random_post['url']
+                await interaction.followup.send(f'{title}\n{url}')
+            else:
+                await interaction.followup.send("No suitable posts found (NSFW filtered)")
+        except KeyError:
+            await interaction.followup.send("The specified subreddit wasn't found, are you sure you spelled it correctly?")
 
     #/gelbooru
     @app_commands.command(name="gelbooru",description="| Scraping | Gets a random image from the specified tags on Gelbooru")
+    @app_commands.describe(tags="What you want to search for, split the tags by using spaces!",ignore_tags="These are the tags you don't want! Split them by using spaces!",safe_mode="Avoids any NSFW images. (Disabling it is only available on NSFW channels)")
     async def gelbooru(self, interaction: discord.Interaction, tags: str, ignore_tags: Optional[str], safe_mode:Literal["Yes","No"]) -> None:
         await interaction.response.defer(ephemeral=False,thinking=True)
-        channel = interaction.channel
-        tags = tags.split(",")
-        if ignore_tags != None:
-            exclude_tags = ignore_tags.split(",")
-            exclude_tags_string = exclude_tags[0]
-            if exclude_tags_string == "" or None:
-                exclude_tags_string = None
+        tags = tags.split(" ")
+        if ignore_tags:
+            exclude_tags = ignore_tags.split(" ")
+        if safe_mode == "Yes":
+            tags.append("rating:general")
         else:
-            exclude_tags = None
-            exclude_tags_string = ""
-        if safe_mode == "No":
-            if interaction.guild and not channel.is_nsfw():
-                await interaction.followup.send("The Safe Mode can only be turned off in a NSFW channel!")
-            else:
-                result = await gelbooru.search_posts(tags=tags,exclude_tags=exclude_tags_string)
-                result = random.choice(result)
-                await interaction.followup.send(result)
-        elif safe_mode == "Yes":
-            async def safe_gelbooru():
-                result = await gelbooru.search_posts(tags=tags,exclude_tags=exclude_tags_string)
-                result = random.choice(result)
-                
-                filename = str(time.strftime('%H')) + str(time.strftime('%M')) + str(time.strftime('%S')) + ".png"
-                img_data = requests.get(result).content
-                with open(filename, 'wb') as handler:
-                    handler.write(img_data)
-                f = open(filename,"rb")
-
-                headers = {"Authorization": f"Authorization: Bearer {os.getenv('edenai_token')}"}
-
-                url=" https://api.edenai.run/v2/image/explicit_content"
-                data={"providers": "api4ai"}
-                files = {'file': f}
-
-                response = requests.post(url, data=data, files=files, headers=headers)
-
-                response = json.loads(response.text)
-                try:
-                    lewd = int(response["api4ai"]["items"][0]["likelihood"])
-                except:
-                    await safe_gelbooru()
-                    return
-                if lewd > 3:
-                    f.close()
-                    try:
-                        os.remove(filename)
-                    except PermissionError:
-                        time.sleep(5)
-                        os.remove(filename)
-                    await safe_gelbooru()
-                    return
-                else:
-                    await interaction.followup.send(result)
-                time.sleep(5)
-                f.close()
-                try:
-                    f.close()
-                    os.remove(filename)
-                except PermissionError:
-                    try:
-                        time.sleep(5)
-                        f.close()
-                    except PermissionError:
-                        try:
-                            os.remove(filename)
-                        except PermissionError:
-                            pass
-                return
-
-            await safe_gelbooru()
-            return
+            if interaction.guild and not interaction.channel.is_nsfw():
+                return await interaction.followup.send("The Safe Mode can only be turned off in a NSFW channel!")
+        result = await gelbooru.search_posts(tags=tags,exclude_tags=exclude_tags if ignore_tags else None)
+        result = random.choice(result)
+        await interaction.followup.send(result)
 
 async def setup(bot: commands.Bot) -> None:
     print("Scraping was loaded!")
