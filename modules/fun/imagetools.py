367c72afaa0a75ff51c6ccc20184f7d79c2039f6
dorkreamer <moonlightdorkreamer@duck.com>
2024-02-15 19:55:55 -0300

    Fixed function quote not scaling images and aligning text poorly

--- modules/fun/imagetools.py
+++ modules/fun/imagetools.py
@@ -1,25 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageEnhance
 import textwrap
 import requests
-from io import BytesIO
+import urllib
 import time
+from io import BytesIO
+import re
 
-def get_pfp(pfp:str,resize=(800,800)):
-    if str(pfp).startswith("https://") or pfp.startswith("http://"):
-        try:
-            response = requests.get(pfp)
-            image = Image.open(BytesIO(response.content))
-        except:
-            return False
+def get_image(image, resize=False, size=(300, 300)):
+    """Checks if the image is a URL and if so, loads it locally, else it tries to load from a path.
+    If `resize` is True, it will resize the image to the specified `size`."""
+    
+    if any(["https://" in image]):
+        response = requests.get(image)
+        image = Image.open(BytesIO(response.content))
     else:
-        try:
-            image = Image.open(pfp)
-        except:
-            return False
-    image = image.convert(mode="RGBA")
+        image = Image.open(image)
+    image = image.convert("RGBA")
     if resize:
-        image = image.resize(resize)
-    return image
+        return image.resize(size)
+    else:
+        return image
+
 
 def quote(id, username, display_name="Anonymous", pfp=None, quote=""):
     quote = "\"" + quote + "\""
@@ -32,7 +33,7 @@ def quote(id, username, display_name="Anonymous", pfp=None, quote=""):
     draw = ImageDraw.Draw(background)
 
     if pfp:
-        pfp = get_pfp(pfp)
+        pfp = get_image(pfp, True, (810, 810))
         background.paste(pfp)
 
     background.paste(gradient,(0,0),gradient)
@@ -41,11 +42,11 @@ def quote(id, username, display_name="Anonymous", pfp=None, quote=""):
     draw.text((980,140),textwrap.fill(quote,35),None,font,align="left",anchor="ms")
 
     #Display name
-    draw.text((915,600),textwrap.fill("- " + display_name,45),None,font,align="left",anchor="ms")
+    draw.text((980,600),textwrap.fill("- " + display_name,45),None,font,align="left",anchor="ms")
 
     if username:
         #Username
-        draw.text((915,630),textwrap.fill("@" + username if not "#" in username else username ,45), None, font3, align="left", anchor="ms")
+        draw.text((980,630),textwrap.fill("@" + username if not re.findall("#", username) else username ,45), None, font3, align="left", anchor="ms")
 
     if __name__ == "__main__":
         background.show()
@@ -76,17 +77,17 @@ def rip(id, username=str, description=None, pfp=None):
     font = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",35)
 
     if pfp:
-        pfp = get_pfp(pfp, (300, 300))
+        pfp = get_image(pfp, (300, 300))
         pfp = ImageEnhance.Contrast(pfp).enhance(2)
         pfp = pfp.filter(ImageFilter.EMBOSS)
         pfp = ImageEnhance.Color(pfp).degenerate
         tombstone.paste(pfp, ((450, 180)), pfp)
 
     text_draw = ImageDraw.Draw(text_layer)
-    text_draw.text((600,130),textwrap.fill(username if username else "Anonymous",30),fill="white",stroke_fill="black",stroke_width=4,font=font,align="center",anchor="ms")
+    text_draw.text((600,130),textwrap.fill(username if username else "Anonymous",30),fill="white",stroke_fill="black",stroke_width=4,font=font,anchor="ms")
     
     if description:
-        text_draw.text((600,517),textwrap.fill(description,30),fill="white",stroke_fill="black",stroke_width=4,font=font,align="center",anchor="ms")
+        text_draw.text((600,517),textwrap.fill(description,30),fill="white",stroke_fill="black",stroke_width=4,font=font,anchor="ms")
     
     text_layer = text_layer.filter(ImageFilter.EMBOSS)
     tombstone.paste(text_layer, text_layer)
@@ -104,4 +105,8 @@ if __name__ == "__main__":
     id = 69
     #rip(id, "Moonlight Dorkreamer 🌓", "Was too much of a dork to be left alive! here's some more text to feasten your eyes!", "./modules/fun/temp/pfp.jpg")
     #dice(id,999)
+    
+    quote(id, "@dorkreamer", "Moonlight Dorkreamer 🌓",f"./modules/fun/temp/pfp.jpg","Okay let's go!")
+    quote(id, "@dorkreamer", "Not Dorkreamer 🌓",f"./modules/fun/temp/pfp.jpg","Okay let's go!")
+    #quote(id, "@dorkreamer", "Moonlight Dorkreamer 🌓",f"./modules/fun/temp/pfp.jpg","OwO!")
     #quote(id, "@dorkreamer", "Moonlight Dorkreamer 🌓",f"./modules/fun/temp/pfp.jpg","I friggin love pudding, anyways here's a long thing OwO, how about we make this even longer and much more lovely, I think we just done it, it's properly wrapping rn")
