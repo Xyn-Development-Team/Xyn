e8b51ce2115fce77a505c4a5f4976c6a466e7762
dorkreamer <moonlightdorkreamer@duck.com>
2024-02-24 17:04:25 -0300

    Added help comments

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
-mode = "Development"
-language = "en-us"
+mode = "Development" #Development, Retail
+language = "en-us" # Check ./localization for the available languages.
 
+#Here you can enable or disable Xyn's modules!
 modules = {
     "fun":True,
     "music":True,
